898f23bee604a3a64406ad4cfe7e1a32e1239fa4
bobbyreed <37113385+bobbyreed@users.noreply.github.com>
2025-04-24 15:00:23 -0500

    Cleaned up some language and started on local save functionality.
    Built pipeline to create a JSON but need to create scene instances of the data that is to be saved.

--- app.gd
+++ app.gd
@@ -14,7 +14,41 @@ func _process(delta: float) -> void:
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
 
+func save():
+	var save_classList = {
+		"filename" : get_scene_file_path(),
+		"parent" : get_parent().get_path()
+	}
+	return save_classList
+	
+func saveData():
+	save()
+	var save_file = FileAccess.open("user://savegame.save", FileAccess.WRITE)
+	var save_nodes = get_tree().get_nodes_in_group("Persist")
+	for node in save_nodes:
+		# Check the node is an instanced scene so it can be instanced again during load.
+		if node.scene_file_path.is_empty():
+			print("persistent node '%s' is not an instanced scene, skipped" % node.name)
+			continue
+
+		# Check the node has a save function.
+		if !node.has_method("save"):
+			print("persistent node '%s' is missing a save() function, skipped" % node.name)
+			continue
+
+		# Call the node's save function.
+		var node_data = node.call("save")
+
+		# JSON provides a static method to serialized JSON string.
+		var json_string = JSON.stringify(node_data)
+
+		# Store the save dictionary as a new line in the save file.
+		save_file.store_line(json_string)
 
 func _on_config_button_pressed() -> void:
 	configWindow.show()
 	pickerPanel.hide()
+
+
+func _on_save_class_button_pressed() -> void:
+	saveData()
